9e786dc26e0d2ffd1de8f2510fcb8f00755ddcec
Junho.choi <105770715+JindoGwen@users.noreply.github.com>
2022-08-17 17:47:09 +0900

    Add files via upload

---
+++ trn04/js/main.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    window.addEventListener('scroll', () => {
+        let sct = window.scrollY;
+        console.log(sct);
+        sct > 0
+            ? document.querySelector('#Header').classList.add('on')
+            : document.querySelector('#Header').classList.remove('on')
+    });
+
+    const MainSlider= new Swiper('.MainSlider', {
+           loop:true,
+           autoplay: {
+            delay:2500,
+            disableOnInteraction: false,
+           },
+           pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+
+        })
+
+
+    const SCE_ELE = document.querySelectorAll('.active-event');
+    window.addEventListener('scroll', () => {
+        let sct = window.scrollY;
+        SCE_ELE.forEach(el => {
+            sct > el.offsetTop -300
+                ? el.classList.add('on')
+                : el.classList.remove('on')
+        })
+    });
+
+    
+
+
+
+
+})
